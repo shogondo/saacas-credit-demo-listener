eafd2a9476bc2ab861de9582d2d6ee252248f335
Shou Takenaka <sasaminn@gmail.com>
2015-04-22 19:33:45 +0900

    Remove unused import directives

--- app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
+++ app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
@@ -1,7 +1,5 @@
 package shou.saacas.demo.listener;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
