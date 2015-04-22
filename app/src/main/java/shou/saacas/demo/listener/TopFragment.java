b3a3ee42659662553a54d63381dad826999de0f1
Shou Takenaka <sasaminn@gmail.com>
2015-04-22 19:42:41 +0900

    Allow to transit from top view to pass over view

--- app/src/main/java/shou/saacas/demo/listener/TopFragment.java
+++ app/src/main/java/shou/saacas/demo/listener/TopFragment.java
@@ -6,9 +6,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.beardedhen.androidbootstrap.BootstrapButton;
+
 public class TopFragment extends Fragment {
+    private BootstrapButton paymentButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.top, container, false);
+        View view = inflater.inflate(R.layout.top, container, false);
+
+        paymentButton = (BootstrapButton)view.findViewById(R.id.payment_button);
+        paymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, new PassOverFragment())
+                        .commit();
+            }
+        });
+
+        return view;
     }
 }
