6dd6ead67ec12acbe90a6073cbab931bc0e0f069
Shou Takenaka <sasaminn@gmail.com>
2015-04-22 19:35:49 +0900

    Remove document comments

--- app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
+++ app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
@@ -7,21 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PassOverFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PassOverFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PassOverFragment extends Fragment {
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment PassOverFragment.
-     */
     public static PassOverFragment newInstance() {
         PassOverFragment fragment = new PassOverFragment();
         Bundle args = new Bundle();
