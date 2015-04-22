83ce39f3875f9953e7c2eaf9696aa613b8fe6b6b
Shou Takenaka <sasaminn@gmail.com>
2015-04-23 01:27:12 +0900

    Enable SSC communication

--- app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
+++ app/src/main/java/shou/saacas/demo/listener/PassOverFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import jp.atrealize.saacas.ssc.Packet;
+import jp.atrealize.saacas.ssc.SSCReceivedStatus;
+import jp.atrealize.saacas.ssc.SaacasSSCListener;
 
 public class PassOverFragment extends Fragment {
     public PassOverFragment() {
@@ -14,5 +17,23 @@ public class PassOverFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.pass_over, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        SaacasSSCListener.getInstance().listen(new SaacasSSCListener.Callback() {
+            @Override
+            public Packet onPacketReceived(Packet packet, SSCReceivedStatus status, int sequence) {
+                Logger.info("SSC packet received (request_code='%s', data='%s', response_code='%s', status='%s', sequence='%d').", packet.getRequestCode(), packet.getData(), packet.getResponseCode(), status, sequence);
+                return packet;
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        SaacasSSCListener.getInstance().close();
     }
 }
